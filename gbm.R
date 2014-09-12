48169fdc1cd514fef4557a136b16836930b8e950
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-13 09:09:25 +1000

    num-nona

--- gbm.R
+++ gbm.R
@@ -33,19 +33,25 @@ nearZeroVar(train,saveMetrics = T)
 train.pca <- preProcess(train[,2:14], method='pca', pcaComp=2)
 
 # model
-fit1 <- train(Label~., method="rf",data=train, prox=T)
+fit1 <- train(Label~., method="rf",data=train)
 fit2 <- train(Label~., method="gbm", data=train)
 fit3 <- train(Label~., method="glm", data=train)
 fit4 <- train(Label~., method="bstTree", data=train)
 fit5 <- train(Label~., method="glmboost", data=train)
 fit6 <- train(Label~., method="nb", data=train)
 
+# load test data
+test <- fread("test.csv", select=c(1:15))
+write.table(test,"test_num.csv",sep=",", row.names=F, col.names=T)
+
+# predict
+gc()
 pred1 <- predict(fit1, test)
 pred2 <- predict(fit2, test)
 pred3 <- predict(fit3, test)
 pred4 <- predict(fit4, test)
 pred5 <- predict(fit5, test)
 pred6 <- predict(fit6, test)
-
+# ensembling-models
 data(pred1,pred2,pred3,pred4,pred5,pred6,train)
 combFit<-train(Label~.,method="gam", train)
