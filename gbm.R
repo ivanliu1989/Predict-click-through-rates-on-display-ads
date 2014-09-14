96aef43f9723faa2419febbf41101f720c74c4d6
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-14 16:34:29 +1000

    glm

--- gbm.R
+++ gbm.R
@@ -1,24 +1,24 @@
 setwd("I:\\data")
 library(data.table)
-train <- fread("train.csv",select=c(1:15))
-head(train)
-write.table(train,"train_num.csv",sep=",", row.names=F, col.names=T)
-str(train)
+    # train <- fread("train.csv",select=c(1:15))
+    # head(train)
+    # write.table(train,"train_num.csv",sep=",", row.names=F, col.names=T)
+    # str(train)
 # rm("train")
-?read.csv
-
-library(caret)
-sum(is.na(train))
-mean(is.na(train))
-summary(train)
-gc()
-train <- fread("train_num.csv")
-train <- na.omit(train)
+    # ?read.csv
+    # 
+    # library(caret)
+    # sum(is.na(train))
+    # mean(is.na(train))
+    # summary(train)
+    # gc()
+    # train <- fread("train_num.csv")
+    # train <- na.omit(train)
 # index <- is.na(train)
 # table(index)
 # train <- train[-index,]
-write.table(train,"train_num_na.csv",sep=",", row.names=F, col.names=T)
-rm(train)
+    # write.table(train,"train_num_na.csv",sep=",", row.names=F, col.names=T)
+    # rm(train)
 
 # data cleansing
 #################
@@ -30,14 +30,14 @@ train$Label <- as.factor(train$Label)
 boxplot(train)
 
 #covariate creation
-nearZeroVar(train,saveMetrics = T)
-train.pca <- preProcess(train[,2:14], method='pca', pcaComp=2)
+    # nearZeroVar(train,saveMetrics = T)
+    # train.pca <- preProcess(train[,2:14], method='pca', pcaComp=2)
 
 # model
-library(doParallel)
+    # library(doParallel)
 library(caret)
-cl <- makePSOCKcluster(4)
-registerDoParallel(cl)
+    # cl <- makePSOCKcluster(4)
+    # registerDoParallel(cl)
 fit1 <- train(Label~., method="rf",data=train)
 
     Grid <- expand.grid(n.trees=c(500),interaction.depth=c(22),shrinkage=.2)
@@ -47,12 +47,17 @@ pred2 <- predict(fit2, train)
 confusionMatrix(pred2, train$Label)
 rm(pred2)
 
-fit3 <- train(Label~., method="nb", data=train)
-stopCluster(cl)
+fit3 <- train(Label~., method="glm",family="binomial" data=train)
+    # stopCluster(cl)
 
 # load test data
-test <- fread("test.csv", select=c(1:14))
-write.table(test,"test_num.csv",sep=",", row.names=F, col.names=T)
+    # test <- fread("test.csv", select=c(1:14))
+    # write.table(test,"test_num.csv",sep=",", row.names=F, col.names=T)
+test <- read.csv("test_num.csv")
+
+# test data imputation
+pre<-preProcess(test, method='knnImpute')
+test_impute <- predict(pre, test)
 
 # predict
 gc()
@@ -66,4 +71,4 @@ combFit<-train(Label~.,method="gam", train)
 # output
 fit2.submit <- data.frame(test$Id, test$pred2)
 colnames(fit2.submit)<- c("Id","Label")
-write.table(fit2.submit,"gbm_num_nona.csv", row.names=F)
+write.table(fit2.submit,"submit1_gbm_num_nona.csv", row.names=F, sep=',')
