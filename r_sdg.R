7b5289654433ac9f0766c4c14a6368f1f381de63
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-18 12:45:50 +1000

    r

--- r_sdg.R
+++ r_sdg.R
@@ -1,18 +1,23 @@
-train <- 'train.csv'
+setwd('/Users/ivan/Work_directory/Predict-click-through-rates-on-display-ads/')
+train <- 'testSet.txt'
 test <- 'test.csv'
 D <- 2^20
 alpha <- .1
-w <- rep(0, n=D)
-n <- rep(0, n=D)
-loss <- 0
+w <- rep.int(0, D)
+n <- rep.int(0, D)
+loss <- 0.
+col_num <- 3
 
-logloss <- function(p,y){
-	p <- max(min(p, 1 - 10e-12), 10e-12)
-    return ifelse(y==1, -log(p), -log(1. - p))
+logloss <- function(p,y) {
+    p <- max(min(p, 1 - 10^-13), 10^-13)
+    res <- ifelse(y==1, -log(p), -log(1 - p))
+    res
 }
-get_data <- function(row, data){
-	r <- read.table(data, colclass=c(rep('Integer', n=41)), skip=row-1, nrows=row, sep=',', colnames=F)
-	return r
+	    
+get_data <- function(row, data, col){
+	r <- read.table(data, 
+                    skip=row-1, nrows=row, sep='\t', col.names=row)
+	r
 }
 
 get_p <- function(x, w){
@@ -20,7 +25,7 @@ get_p <- function(x, w){
 	for i in 1:x
 		wTx <- wTx + w[i] * 1.
     p <- 1/(1 + exp(-max(min(wTx, 20), -20)))
-    return p 
+    p 
 }
 
 update_w <- function(w, n, x, p, y){
@@ -30,12 +35,18 @@ update_w <- function(w, n, x, p, y){
     return w, n
 }
 
-
-
-
-
-
 for i in 1:46000000{
-	
-
+	x <- get_data(i, train)
+	y <- x[2]
+	x <- x[-c(1,2)]
+	p <- get_p(x, w)
+	loss <- loss + logloss(p,y)
+	if(i%1000000 == 0){
+		'logloss: '&loss/i&'. number of row: 'i
+	}
+	updates <- update_w(w, n, x, p, y)
+	w <- updates[1]
+	n <- updates[2]
 }
+
+w
