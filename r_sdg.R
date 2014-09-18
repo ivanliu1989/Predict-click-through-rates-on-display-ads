c0b65436b0e1d48b35486f4e59a18e449db57f29
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-18 15:13:55 +1000

    r

--- r_sdg.R
+++ r_sdg.R
@@ -8,38 +8,44 @@ n <- rep.int(0, D)
 loss <- 0.
 col_num <- 3
 
+# test logloss of predictions and true values
 logloss <- function(p,y) {
     p <- max(min(p, 1 - 10^-13), 10^-13)
     res <- ifelse(y==1, -log(p), -log(1 - p))
     res
 }
-	    
-get_data <- function(row, data, col){
-	r <- read.table(data, 
-                    skip=row-1, nrows=row, sep='\t', col.names=row)
-	r
+
+# extract one record from database	    
+get_data <- function(row, data){
+	r <- read.table(data, skip=row-1, nrows=1, sep='\t', row.names=row)
+    r
 }
 
+# get possibilities of records
 get_p <- function(x, w){
 	wTx <- 0
-	for i in 1:x
-		wTx <- wTx + w[i] * 1.
+	for (i2 in x) {
+		wTx <- wTx + w[i2] * 1.}
     p <- 1/(1 + exp(-max(min(wTx, 20), -20)))
     p 
 }
 
+# update the weights according to results
 update_w <- function(w, n, x, p, y){
-	for i in 1:x
+	for (i in x){
         w[i] <- w[i] - ((p - y) * alpha / (sqrt(n[i]) + 1))
         n[i] <- n[i] + 1
-    return w, n
+	}
+    w
+    n
 }
 
+# main steps for modeling
 for i in 1:46000000{
-	x <- get_data(i, train)
-	y <- x[2]
-	x <- x[-c(1,2)]
-	p <- get_p(x, w)
+	row <- get_data(i, train,2)
+	y <- row[2]
+	row <- row[-c(1,2)]
+	p <- get_p(row, w)
 	loss <- loss + logloss(p,y)
 	if(i%1000000 == 0){
 		'logloss: '&loss/i&'. number of row: 'i
@@ -49,4 +55,16 @@ for i in 1:46000000{
 	n <- updates[2]
 }
 
-w
+
+get_x <- function(row, D){
+    x <- c(0)
+    for (j in row){
+        index <- as.integer(j) %% D
+        x <- c(x, index)
+    }
+}
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-from numpy import *
-dataMat = []; labelMat = []
-fr = open('testSet.txt')
-for line in fr.readlines():
-    singleArr=[1.0]
-    lineArr = line.strip().split()
-    for i in range(2):
-        singleArr.append(float(lineArr[i]))
-    dataMat.append(singleArr)
-    labelMat.append(int(lineArr[2]))
-
-print ones(18)
+print 102%2**20
+print 2**20
