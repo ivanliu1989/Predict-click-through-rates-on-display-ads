249d2ebbbfbfe3911cf6416575570a65b09d62fa
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-24 19:30:31 +1000

    winners models

--- pythonSGD/py_lh4_22Sep2014.py
+++ pythonSGD/py_lh4_22Sep2014.py
@@ -8,7 +8,7 @@ import mmh3
 train = 'train.csv'  # path to training file
 test = 'test.csv'  # path to testing file
 
-D = 2 ** 27  # number of weights use for learning
+D = 2 ** 28  # number of weights use for learning
 alpha = .145    # learning rate for sgd optimization
 
 
@@ -21,7 +21,7 @@ alpha = .145    # learning rate for sgd optimization
 # OUTPUT
 #     logarithmic loss of p given y
 def logloss(p, y):
-    p = max(min(p, 1. - 10e-12), 10e-12)
+    p = max(min(p, 1. - 10e-15), 10e-15)
     return -log(p) if y == 1. else -log(1. - p)
 
 
@@ -35,7 +35,7 @@ def logloss(p, y):
 def get_x(csv_row, D):
     x = [0]  # 0 is the index of the bias term
     for key, value in csv_row.items():
-        index = mmh3.hash(value + key[1:]) % D  # weakest hash ever ;)
+        index = mmh3.hash((value + key[1:]),42) % D  # weakest hash ever ;)
         x.append(index)
     return x  # x contains indices of features that have a value of 1
 
@@ -46,11 +46,12 @@ def get_x(csv_row, D):
 #     w: weights
 # OUTPUT:
 #     probability of p(y = 1 | x; w)
-def get_p(x, w):
+def get_p(x, w, ld):
+    ld = ld + 0.001
     wTx = 0.
     for i in x:  # do wTx
         wTx += w[i] * 1.  # w[i] * x[i], but if i in x we got x[i] = 1.
-    return 1. / (1. + exp(-max(min(wTx, 20.), -20.)))  # bounded sigmoid
+    return (1.+ld) / (1. + exp(-max(min(wTx, 20.), -20.))) # bounded sigmoid
 
 
 # D. Update given model
@@ -83,7 +84,9 @@ n = [0.] * D  # number of times we've encountered a feature
 
 # start training a logistic regression model using on pass sgd
 loss = 0.
+ld = 0.001
 for t, row in enumerate(DictReader(open(train))):
+
     y = 1. if row['Label'] == '1' else 0.
 
     del row['Label']  # can't let the model peek the answer
@@ -94,7 +97,7 @@ for t, row in enumerate(DictReader(open(train))):
     x = get_x(row, D)
 
     # step 2, get prediction
-    p = get_p(x, w)
+    p = get_p(x, w, ld)
 
     # for progress validation, useless for learning our model
     loss += logloss(p, y)
@@ -106,7 +109,7 @@ for t, row in enumerate(DictReader(open(train))):
     w, n = update_w(w, n, x, p, y)
 
 # testing (build kaggle's submission file)
-with open('submissionPython5.csv', 'w') as submission:
+with open('submissionPython22Sep2014_pm.csv', 'w') as submission:
     submission.write('Id,Predicted\n')
     for t, row in enumerate(DictReader(open(test))):
         Id = row['Id']
